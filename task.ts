89f5e018a3bd2c738e6ef22fdde37046b8ae4afd
ingalls <nick@ingalls.ca>
2024-01-01 18:02:30 -0700

    Setup Lat/Lng parsing

--- task.ts
+++ task.ts
@@ -81,8 +81,8 @@ export default class Task extends ETL {
             agencyForm.append('auth', login.jwt);
             agencyForm.append('post_data', JSON.stringify({
                 agency_id: agency.id,
-                from_date: 1703487600000,
-                to_date: 1704092400000
+                from_date: moment().subtract(6, 'hours').unix() * 1000,
+                to_date:   moment().unix() * 1000
             }));
 
             const alerts = JSON.parse((await (await fetch("https://interface.active911.com/interface/interface.ajax.php", {
@@ -98,14 +98,11 @@ export default class Task extends ETL {
             const parsed = parse(alerts, { columns: true });
 
             for (const p of parsed) {
-                console.error('address', p.address);
-                console.error('place', p.place);
-                console.error('lat/lon', p.lat, p.lon);
                 fc.features.push({
                     id: `active911-${p.id}`,
                     type: 'Feature',
                     properties: {
-                        callsign: p.description,
+                        callsign: `911: ${p.description}`,
                         time: moment(p.send).toISOString(),
                         remarks: `
                             Groups: ${p.units}
@@ -115,7 +112,7 @@ export default class Task extends ETL {
                     },
                     geometry: {
                         type: 'Point',
-                        coordinates: []
+                        coordinates: [Number(p.lon), Number(p.lat)]
                     }
                 });
             }
